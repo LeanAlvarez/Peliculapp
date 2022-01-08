e0439ccc384eb963a6a0c5a718720b9bd9930465
lean alvarez <leanalvarez.dev@gmail.com>
2022-01-08 10:57:38 -0300

    creacion del modelo movie y de los metodos del MovieDataSource de la capa de datos

--- app/src/main/java/com/softmirai/peliculapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/softmirai/peliculapp/data/remote/MovieDataSource.kt
@@ -1,4 +1,21 @@
 package com.softmirai.peliculapp.data.remote
 
+import com.softmirai.peliculapp.data.model.MovieList
+
 class MovieDataSource {
+    //creo los metodos que van a ir a buscar información al servidor de cada una de las peliculas
+
+    fun getUpcomingMovies(): MovieList{
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList{
+        return  MovieList()
+    }
+
+
+    fun getPopularMovies(): MovieList{
+        return MovieList()
+    }
+
 }
