37893253b1ae9611e08384e39eb138e98eb3458b
lean alvarez <leanalvarez.dev@gmail.com>
2022-01-08 13:15:25 -0300

    finalizo la capa de repositorio

--- app/src/main/java/com/softmirai/peliculapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/softmirai/peliculapp/data/remote/MovieDataSource.kt
@@ -1,21 +1,19 @@
 package com.softmirai.peliculapp.data.remote
 
+import com.softmirai.peliculapp.application.AppConstants
 import com.softmirai.peliculapp.data.model.MovieList
+import com.softmirai.peliculapp.repository.WebService
 
-class MovieDataSource {
+class MovieDataSource(private val webService: WebService) {
     //creo los metodos que van a ir a buscar información al servidor de cada una de las peliculas
 
-    fun getUpcomingMovies(): MovieList{
-        return MovieList()
-    }
-
-    fun getTopRatedMovies(): MovieList{
-        return  MovieList()
-    }
+    suspend fun getUpcomingMovies(): MovieList = webService.getUpcomingMovies(AppConstants.API_KEY)
 
 
-    fun getPopularMovies(): MovieList{
-        return MovieList()
-    }
+    suspend fun getTopRatedMovies(): MovieList = webService.getTopRatedMovies(AppConstants.API_KEY)
+
+
+    suspend fun getPopularMovies(): MovieList = webService.getPopularMovies(AppConstants.API_KEY)
+
 
 }
