7b4a29b174d87b7f1c269d42eea2558fb9ac7a71
lean alvarez <leanalvarez.dev@gmail.com>
2022-01-08 12:43:21 -0300

    implementacion de co-rutinas para la busqueda de info en el servidor

--- app/src/main/java/com/softmirai/peliculapp/repository/MovieRepository.kt
+++ app/src/main/java/com/softmirai/peliculapp/repository/MovieRepository.kt
@@ -1,4 +1,18 @@
 package com.softmirai.peliculapp.repository
 
+import com.softmirai.peliculapp.data.model.MovieList
+
 interface MovieRepository {
+
+    /*defino los metodos que luego voy a ocupar en el data source
+
+     utilizo co-rutinas para ir a buscar la info al server
+
+     */
+
+    suspend fun getUpcomingMovies(): MovieList
+
+    suspend fun getTopRatedMovies(): MovieList
+
+    suspend fun getPopularMovies(): MovieList
 }
--- app/src/main/java/com/softmirai/peliculapp/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/softmirai/peliculapp/repository/MovieRepositoryImpl.kt
@@ -1,4 +1,17 @@
 package com.softmirai.peliculapp.repository
 
+import com.softmirai.peliculapp.data.model.MovieList
+
 class MovieRepositoryImpl : MovieRepository{
+    override suspend fun getUpcomingMovies(): MovieList {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTopRatedMovies(): MovieList {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getPopularMovies(): MovieList {
+        TODO("Not yet implemented")
+    }
 }
