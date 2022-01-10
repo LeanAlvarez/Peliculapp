85e55e0834542326a8c7f1e72c038d15b930e13c
lean alvarez <leanalvarez.dev@gmail.com>
2022-01-10 20:27:07 -0300

    creacion del package de core con los genericos y el liveData

--- app/src/main/java/com/softmirai/peliculapp/presentation/MovieViewModel.kt
+++ app/src/main/java/com/softmirai/peliculapp/presentation/MovieViewModel.kt
@@ -1,4 +1,33 @@
 package com.softmirai.peliculapp.presentation
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.softmirai.peliculapp.core.Resource
+import com.softmirai.peliculapp.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+
+class MovieViewModel(private val repo: MovieRepository ): ViewModel() {
+    //metodo para ir a buscar las ultimas peliculas
+
+    fun fetchUpcomingMovies() = liveData(Dispatchers.IO){
+
+        emit(Resource.Loading())
+
+        try {
+            emit(Resource.Success(repo.getUpcomingMovies()))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+
+    }
+
+}
+
+
+class MovieViewModelFactory(private val repo: MovieRepository): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
+    }
+
 }
