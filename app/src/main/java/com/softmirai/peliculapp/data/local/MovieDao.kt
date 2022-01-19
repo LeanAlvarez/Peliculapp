86c5a49420829e9652237cc58cf59d7be8a74bdb
lean alvarez <leanalvarez.dev@gmail.com>
2022-01-19 17:15:12 -0300

    DAO

---
+++ app/src/main/java/com/softmirai/peliculapp/data/local/MovieDao.kt
@@ -0,0 +1,21 @@
+package com.softmirai.peliculapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.softmirai.peliculapp.data.model.MovieEntity
+
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movieentity")
+    suspend fun getAllMovies():List<MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMovie(movie:MovieEntity)
+
+
+
+}
