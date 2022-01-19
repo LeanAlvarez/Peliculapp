3b0b6188257911f19a49fe1d9f6f528ecaa9773b
lean alvarez <leanalvarez.dev@gmail.com>
2022-01-19 18:28:59 -0300

    AppDatabase

---
+++ app/src/main/java/com/softmirai/peliculapp/data/local/AppDatabase.kt
@@ -0,0 +1,39 @@
+package com.softmirai.peliculapp.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.softmirai.peliculapp.data.model.MovieEntity
+
+
+@Database(entities = [MovieEntity::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
+
+
+    //creo la instancia de room
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase{
+            INSTANCE = INSTANCE ?: Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "movie_table"
+            ).build()
+            return INSTANCE!!
+        }
+
+        fun destroyInstance(){
+            INSTANCE = null
+        }
+
+
+
+    }
+
+
+}
